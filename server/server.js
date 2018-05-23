c470453f9e3e2bd0235fcfa22a552d5913746285
wei <wei@weis-MacBook-Air.local>
2018-05-23 06:54:09 -0500

    Create a new user and alert others

--- server/server.js
+++ server/server.js
@@ -35,11 +35,28 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('new message from client', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
+
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to the chat app.',
             createdAt: new Date().getTime()
         });
+
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New user joined',
+            createdAt: new Date().getTime()
+        });
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
