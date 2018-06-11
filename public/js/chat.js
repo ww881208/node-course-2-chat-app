0839ad41090e8ec261e1a217104d861ad2eb71a9
wei <wei@weis-MacBook-Air.local>
2018-06-11 06:45:01 -0500

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -115,7 +115,7 @@ jQuery('#message-form').on('submit', function(e){
 
     var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
-        from: 'User',
+        //from: 'User',
         text: messageTextBox.val()
     }, function(){
         messageTextBox.val('');
--- server/server.js
+++ server/server.js
@@ -60,8 +60,14 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        //console.log('createMessage', message);
+
+        var user = users.getUser(socket.id);
+
+        if(user && isRealString(message.text)){
+            //io.emit('newMessage', generateMessage(message.from, message.text)); //this sends message to everyone regardless chat rooms
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text)); //send message to specific room 
+        }
         callback();
 
 
@@ -84,8 +90,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-        //console.log(coords.longitude);
+        var user = users.getUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+            //console.log(coords.longitude);
+        }
     })
 
     socket.on('disconnect', () => {
